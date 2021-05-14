d14afc163dbbafa90a00e596a4f720f231140b4d
kamkara <lnclass.kamkara@gmail.com>
2021-05-14 11:37:32 +0000

    add title on onglet and login anywhere

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+def title
+    base_title = "LNCLASS"
+    if @title.nil?
+       base_title
+    else
+     " #{@title} - #{base_title}"
+    end
+end
+#Sign in and up anywhere on app
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
