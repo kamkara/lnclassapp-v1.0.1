a25b3efddd37ecd5babb68d0ec126bf912398e7f
kamkara <lnclass.kamkara@gmail.com>
2021-05-15 09:27:59 +0000

    add course build

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,12 +1,25 @@
 class HomesController < ApplicationController
-   before_action :find_levels
+  before_action :find_levels
   before_action :find_materials
+  #before_filter :find_user_content_access
+
 
   def index
     @materials = Material.all.order('created_at desc')
     @courses   = Course.all.order('created_at desc')
   end
 
+  def bepec
+    
+  end
+
+  def bac_a
+    
+  end
+
+  def bac_d
+
+  end
 
   private
     def find_levels
@@ -15,5 +28,8 @@ class HomesController < ApplicationController
 
     def find_materials
       @materials = Material.all
+    end
+    def find_user_content_access
+      
     end
 end
