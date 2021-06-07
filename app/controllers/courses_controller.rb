abedf5a4f02200ead772fe3aa5bf07626d9c9e7d
kamkara <lnclass.kamkara@gmail.com>
2021-06-07 22:33:03 +0000

    add home page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -55,7 +55,7 @@ class CoursesController < ApplicationController
   def destroy
     @course.destroy
     respond_to do |format|
-      format.html { redirect_to courses_url, notice: "Course was successfully destroyed." }
+      format.html { redirect_to course_path(@course), notice: "Course was successfully destroyed." }
       format.json { head :no_content }
     end
   end
