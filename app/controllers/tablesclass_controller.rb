7acda2267fffbf7520fbf55b21b8bcb70a5f9040
kamkara <lnclass.kamkara@gmail.com>
2021-06-09 16:04:24 +0000

    add content role

--- app/controllers/tablesclass_controller.rb
+++ app/controllers/tablesclass_controller.rb
@@ -1,4 +1,23 @@
 class TablesclassController < ApplicationController
+   before_action :authenticate_user!
+   before_action :find_courses
+   before_action :find_posts
+   before_action :find_messages
+
   def index
+    @courses = Course.all
+    @posts =Post.all
+    @messages = Message.all
   end
+private
+  #enable level
+    def find_courses
+      @courses = Course.all
+    end
+    def find_posts
+      @posts = Post.all
+    end
+    def find_messages
+      @messages = Message.all
+    end
 end
