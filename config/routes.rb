6a58c23b70076a0b5720f23791fecf5172d9f945
kamkara <lnclass.kamkara@gmail.com>
2021-06-09 09:56:35 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unacceptable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
