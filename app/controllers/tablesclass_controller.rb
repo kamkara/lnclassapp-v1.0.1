e63b127ead3a2ae1879041d537465f9372816997
kamkara <lnclass.kamkara@gmail.com>
2021-06-10 10:55:11 +0000

    add statitis

--- app/controllers/tablesclass_controller.rb
+++ app/controllers/tablesclass_controller.rb
@@ -1,4 +1,19 @@
 class TablesclassController < ApplicationController
+before_action :authenticate_user!
+before_action :set_course
+before_action :set_users
+
   def index
+    @NmbreCourse = Course.all
+    @NmbreUser    = User.all
   end
+
+  private
+    def set_course
+      @course = Course.all
+    end
+
+    def set_users
+      @user = User.all
+    end
 end
