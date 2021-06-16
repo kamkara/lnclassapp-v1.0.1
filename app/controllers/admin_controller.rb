4425b2a07c2f75628e4bc3d0da4d6b220708a780
kamkara <lnclass.kamkara@gmail.com>
2021-06-16 09:05:36 +0000

    add admin space

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,25 @@
 class AdminController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :find_materials#, only: [:index, :show, :new, :edit, :create, :update]
+  before_action :find_levels#, only:    [:index, :show, :new, :edit, :create, :level]
+  before_action :find_courses#, only: %i[ show edit update destroy ]
+
   def index
   end
+
+  private
+  #enable material
+    def find_materials
+      @materials = Material.all
+    end
+
+    #enable level
+    def find_levels
+      @levels = Level.all
+    end
+    #enable level
+    def find_courses
+      @courses = Course.all
+    end
 end
