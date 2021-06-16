7accd26bf3d5930d378997d40f800a592f20aa4b
kamkara <lnclass.kamkara@gmail.com>
2021-06-16 12:31:59 +0000

    add admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,28 @@
 class AdminController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :find_materials#, only: [:index, :show, :new, :edit, :create, :update]
-  before_action :find_levels#, only:    [:index, :show, :new, :edit, :create, :level]
-  before_action :find_courses#, only: %i[ show edit update destroy ]
+  before_action :find_materials
+  before_action :find_levels
+  before_action :find_courses
+  before_action :find_posts
+  before_action :find_messages
 
   def index
+    
+    @Nb_sujets          = Course.all
+    @NmbrePosts         =Post.all
+    @NmbreMessages      = Message.all
+    @NmbreUsers         = User.all
+    @NmbreDailyCourses  = Course.where(:created_at => 1.day.ago..Time.now)
+    @NmbreDailyMessages = Message.where(:created_at => 1.day.ago..Time.now)
+    @NmbreDailyPosts    = Post.where(:created_at => 1.day.ago..Time.now)
+    @NbDailyUsers       = User.where(:created_at => 1.day.ago..Time.now)
+    @NbDailyUsersByCity = User.where(:created_at => 1.day.ago..Time.now)
+    @StudentsByCity        = User.where("city= ?  AND :role= ?", current_user.city, "Student")
+    @ProfsByCity        = User.where("city= ?  AND :role= ?", current_user.city, "Teacher")
+    @SuperProfsByCity   = User.where("city= ?  AND :role= ?", current_user.city, "Super prof")
+    @DailyUsersByCity   = User.where("create_at= ?  AND :city=?", "1.day.ago..Time.now",  current_user.city)
+    @DailyUsers         = User.where(:current_sign_in_at => 1.day.ago..Time.now)
   end
 
   private
@@ -21,5 +38,11 @@ class AdminController < ApplicationController
     #enable level
     def find_courses
       @courses = Course.all
+    end
+    def find_posts
+      @posts = Post.all
+    end
+    def find_messages
+      @messages = Message.all
     end
 end
