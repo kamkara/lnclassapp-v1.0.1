45599b130b4ad722e5a648b868c9c642f97609fc
kamkara <lnclass.kamkara@gmail.com>
2021-06-28 15:55:44 +0000

    add Abonnement message on home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_174852) do
+ActiveRecord::Schema.define(version: 2021_06_28_151538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -155,6 +155,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_174852) do
     t.string "role", default: "student"
     t.string "slug"
     t.datetime "deleted_at"
+    t.string "status", default: "inscrit"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone_contact"], name: "index_users_on_phone_contact", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
