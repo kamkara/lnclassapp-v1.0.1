6eec798cd274eecbb7dad4c41cfa7abefd611e8a
kamkara <lnclass.kamkara@gmail.com>
2021-06-28 16:30:59 +0000

    add email generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,46 +23,57 @@ class User < ApplicationRecord
   before_save :school_name
 
   
+  
 
-
-######### PRESENTES && FORMAT  ######
-validates :phone_contact,
-          :city, :email,
-            presence: true
-validates :password, length: { in: 8..45 }
+  ######### PRESENTES && FORMAT  ######
+  validates :phone_contact,
+  :city, :email,
+  presence: true
+  validates :password, length: { in: 8..45 }
 validates :last_name, :first_name, 
-          presence: true,
-          length: { maximum: 50 },
-          format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ }
+presence: true,
+length: { maximum: 50 },
+format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ }
 
 validates :phone_contact,
-          length: { in: 10..12 },
-          numericality: { only_integer: true },
-          uniqueness: true
-          
+length: { in: 10..12 },
+numericality: { only_integer: true },
+uniqueness: true
+
 validates_inclusion_of :role, :in => ["City Manager", "Marketing", "Head of City Manager", "Content", "Head of Content", "Teacher", "Student", "Super prof"]
-          
-          
+
+
 ################  CONSTANTE   ###########################
-  #SCHOOL_NAME = ["LYCEE MODERNE TIASSALE", "COLLEGE SAINT MICHEL TIASSALE", "COLLEGE PRIVE MIXTE UNION TIASSALE", "COLLÈGE NOTRE DAME DE LA PAIX TIASSALE", "COLLÈGE PRIVE LA MANNE", "Autres villes"]
+#SCHOOL_NAME = ["LYCEE MODERNE TIASSALE", "COLLEGE SAINT MICHEL TIASSALE", "COLLEGE PRIVE MIXTE UNION TIASSALE", "COLLÈGE NOTRE DAME DE LA PAIX TIASSALE", "COLLÈGE PRIVE LA MANNE", "Autres villes"]
 
   ROLE_NAME   = ["City manager", "Marketing", "Head of City Manager", "Content", "Head of Content", "Super prof"]
   
   CITY_NAME   = ["Choisissez votre ville","Tiassalé", "N'Douci","Agboville","Abidjan", "Hermankono", "Divo", "Odienné", "Duékoué", "Danané", "Tingréla", "Bouaké", "Daloa", "Yamoussoukro", "San-Pédro", "Abengourou", "Man", "Gagnoa", "Katiola",
-                "Korhogo", "Dabou", "Divo","Grand-Bassam", "Bouaflé", "Issia", "Sinfra",  "Abengourou" ,"Soubré", 
-                "Adzopé", "Séguéla", "Bondoukou", "Oumé", "Ferkessedougou", "Dimbokro",
-                "Guiglo", "Boundiali", "Agnibilékrou", "Daoukro", "Vavoua", "Zuénoula", "Toumodi", "Akoupé", "Lakota", "Autres villes"]
+    "Korhogo", "Dabou", "Divo","Grand-Bassam", "Bouaflé", "Issia", "Sinfra",  "Abengourou" ,"Soubré", 
+    "Adzopé", "Séguéla", "Bondoukou", "Oumé", "Ferkessedougou", "Dimbokro",
+    "Guiglo", "Boundiali", "Agnibilékrou", "Daoukro", "Vavoua", "Zuénoula", "Toumodi", "Akoupé", "Lakota", "Autres villes"]
+    
+    ############# selfs variables  ############   
+    def school_name
+      self.school_name = "QG LNCLASS"
+    end 
+    
+    def email
+      self.email = "#{self.phone_contact}@gmail.com"
+    end
+    def student_matricule
+      if self.role === "Student"
+        validates :matricule, uniqueness: true, length: { is: 9 }
+      end
+    end
+    ############ SLUG ###########
+    def slug
+      self.slug = self.full_name
+    end
 
-  
- def school_name
-   self.school_name = "QG LNCLASS"
- end 
-########### role default value= STUDENT  ######
-def student_matricule
-  if self.role === "Student"
-    validates :matricule, uniqueness: true, length: { is: 9 }
-  end
-end
+    def user_full_name
+      self.full_name = "#{self.first_name} #{self.last_name}"
+    end
 
 
 ################  SIGN IN PHONE NUMBR OR EMAIL  ###########################
@@ -89,27 +100,17 @@ def assign_user_role
   add_role(:Content) if self.matricule == "" && self.role == "Content"
   add_role(:Admin) if self.matricule == "" && self.role == "Admin"
   self.save!
-  end
-  
-  def user_full_name
-    self.full_name = "#{self.first_name} #{self.last_name}"
-  end
+end
 
-  
 
-  def normalize_fields
-    self.phone_contact      = phone_contact.strip.squeeze(" ")
-    #self.whatsapp_contact   = whatsapp_contact.strip.squeeze(" ")
-    self.first_name         = first_name.strip.squeeze(" ").downcase.capitalize
-    self.last_name          = last_name.strip.squeeze(" ").downcase.capitalize
-    self.city               = city.strip.squeeze(" ").downcase.capitalize
+def normalize_fields
+  self.phone_contact      = phone_contact.strip.squeeze(" ")
+  #self.whatsapp_contact   = whatsapp_contact.strip.squeeze(" ")
+  self.first_name         = first_name.strip.squeeze(" ").downcase.capitalize
+  self.last_name          = last_name.strip.squeeze(" ").downcase.capitalize
+  self.city               = city.strip.squeeze(" ").downcase.capitalize
   end
   
-  
-  ############ SLUG ###########
-  def slug
-    self.slug = self.full_name
-  end
   
   extend FriendlyId
   friendly_id :slug, use: :slugged
